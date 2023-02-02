c7073a110f11da189dec9c88ee957d143bd60b62
Daisy-Violet <a.laska.yang@mail.ru>
2023-02-02 17:30:15 +0300

    Add amendments

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let errors = [];
 function checkValidity(input) {
     let validity = input.validity;
-    if (!validity.valueMissing) { errors.push('Поле ' + input.placeholder + ' не заполнено'); }
-    if (!validity.patternMismatch) { errors.push('Неверный формат заполнения'); }
+    if (validity.valueMissing) { errors.push('Поле ' + input.placeholder + ' не заполнено'); }
+    if (validity.patternMismatch) { errors.push('Неверный формат заполнения'); }
     if (validity.rangeUnderflow) {
         let min = getAttributeValue(input, 'min');
         errors.push('Минимальное значение не может быть больше чем ' + min);

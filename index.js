7c339c6ccdfe7004ee5339b06fddbf3a5a2a6576
Daisy-Violet <a.laska.yang@mail.ru>
2023-02-02 14:40:55 +0300

    Add amendments

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 let errors = [];
 function checkValidity(input) {
     let validity = input.validity;
-    if (validity.valueMissing) { errors.push('Поле ' + input.placeholder + ' не заполнено'); }
-    if (validity.patternMismatch) { errors.push('Неверный формат заполнения'); }
-
-    if (validity.rangeOverflow) {
-        let max = getAttributeValue(input, 'max');
-        errors.push('Максимальное значение не может быть больше чем ' + max);
-    }
-
+    if (!validity.valueMissing) { errors.push('Поле ' + input.placeholder + ' не заполнено'); }
+    if (!validity.patternMismatch) { errors.push('Неверный формат заполнения'); }
     if (validity.rangeUnderflow) {
         let min = getAttributeValue(input, 'min');
         errors.push('Минимальное значение не может быть больше чем ' + min);
@@ -22,5 +16,5 @@ function checkAll() {
         checkValidity(input);
     }
     let result = document.getElementById("errorsInfo");
-    result.innerHTML = errors.join('. \n');
+    result.innerHTML = errors.join('. <br>');
 }
